3a44f707ca521bdd9b259e3716b93595e9bf85dc
agusle <agustinleperini5@gmail.com>
2022-11-28 14:16:04 -0300

    Cambios en titulo de respuesta y valores iniciales de variables SHORTEST y LONGEST

--- statsWords.sh
+++ statsWords.sh
@@ -8,9 +8,9 @@
 FILE=$1
 
 TOTAL_WORDS=$(cat $FILE | wc -w)
-SHORTEST=10
+SHORTEST=100
 SHORTEST_W=""
-LONGEST=1
+LONGEST=-1
 LONGEST_W=""
 LENGTH_SUM=0
 
@@ -22,11 +22,11 @@ do
 done
 echo ""
 echo "=============================================================================================="
-echo "                        LONGITUD DE PALABRAS DE $FILE                                         "
+echo "                        LONGITUD DE PALABRAS DE \"$(ls ./data)\"                                        "
 echo "=============================================================================================="
 echo ""
 echo "PALABRA MAS CORTA: \"$SHORTEST_W\" con $SHORTEST caracteres."
 echo "PALABRA MAS LARGA: \"$LONGEST_W\" con $LONGEST caracteres."
-echo "EL PROMEDIO DE LONGITUD DE PALABRAS ES: $(echo "scale=0 ; $LENGTH_SUM/$TOTAL_WORDS" | bc) caracteres."
+echo "PROMEDIO DE LONGITUD DE PALABRAS: $(echo "scale=0 ; $LENGTH_SUM/$TOTAL_WORDS" | bc) caracteres."
 echo ""
 
