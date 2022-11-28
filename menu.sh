9c45df52721aa1f7822b6d241bb7a3201eff4ef2
agusle <agustinleperini5@gmail.com>
2022-11-27 23:05:05 -0300

    Validacion para que se encuentre un solo archivo en el directiorio ./data agregada y cambio en los mensajes de exit

--- menu.sh
+++ menu.sh
@@ -8,8 +8,9 @@
 #read -p "Antes de comenzar, ¿podrias decirme tu nombre? : " USERNAME
 
 #validacion de archivo de texto en directorio  ./data
-[[ $(ls ./data | wc -l) -lt 1 ]] && echo "No se encontró archivo dentro del directorio ./data" && exit 1 
-[[ ! -e $(find ./data -type f -name *.txt) ]] && echo "El archivo cargado en ./data  no es de texto." && exit 2
+[[ $(ls ./data | wc -l) -lt 1 ]] && echo "No se encontró archivo dentro del directorio \"./data\"." && exit 1 
+[[ $(ls ./data | wc -l) -gt 1 ]] && echo "El directorio \"./data\" posee mas de un archivo." && exit 2 
+[[ ! -e $(find ./data -type f -name *.txt) ]] && echo "El archivo cargado en ./data  no es de texto." && exit 3
 
 #mensaje de bienvenida
 echo -e "Hola $USERNAME!\nBienvenido al analizador de textos con bash!"
